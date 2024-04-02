1e7dbc080e6b0549533fcd373f2f2bfff22bcd83
Lauri Kodisoja <laurikodisoja@windowslive.com>
2024-04-02 12:16:38 +0300

    added .gitignore

---
+++ task_manager_app.py
@@ -0,0 +1,13 @@
+# File:         task_manager_app.py
+# Author:       Lauri Kodisoja
+# Description:  task manager application
+
+from users import User, TaskManager
+
+
+"""Class definition for the application"""
+
+class TaskManagerApp:
+
+    def __init__(self):
+        self.
