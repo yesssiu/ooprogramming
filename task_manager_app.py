2b15310dbcae7e9b9685d4af1b0d1d8817aa9b77
Lauri Kodisoja <laurikodisoja@windowslive.com>
2024-04-10 10:43:55 +0300

    quick commit so can update gui branch

--- task_manager_app.py
+++ task_manager_app.py
@@ -31,6 +31,7 @@ class TaskManagerApp:
         print("2 Own tasks")
         print("3 All tasks")
         print("4 New task")
+        print("5 View all users")
         print()
 
     # menu for admin users
@@ -286,6 +287,10 @@ class TaskManagerApp:
 
                 elif command == "4":
                     self.add_task_user()
+                    print()
+
+                elif command == "5":
+                    self.view_users()
                     print()
 
                 else:
